416e7bcb69f719c18d396956d99db4dfbf785389
ldiur <lioneldiur@icloud.com>
2020-05-16 00:40:54 +0200

    Post job bar homepage

--- src/container/MainHome/MainHome.js
+++ src/container/MainHome/MainHome.js
@@ -1,41 +1,43 @@
-import React, { Component } from 'react'
-import './MainHome.css'
-import UserProfiction from '../../components/UserProfiction/UserProfiction'
-import PostJob from "../../components/PostJob/PostJob"
-import AllPost from '../../components/AllPost/AllPost'
-import  TopJobs from '../../components/TopJobs/TopJobs'
-import MostViews from '../../components/MostViewd/MostView'
+import React, { Component } from "react";
+import "./MainHome.css";
+import UserProfiction from "../../components/UserProfiction/UserProfiction";
+import PostJob from "../../components/PostJob/PostJob";
+import AllPost from "../../components/AllPost/AllPost";
+import TopJobs from "../../components/TopJobs/TopJobs";
+import MostViews from "../../components/MostViewd/MostView";
 
 export class Main extends Component {
-    render() {
-        return (
-            <main className="main-section" >
-                <div className="container">
-                    <div className="main-section-data">
-                        <div className="row">
-
-                        <div className="col-lg-3 col-md-4 pd-left-none no-pd">
-                        <div >
-                            <UserProfiction/>
-                        </div>
-                        </div>
-                            <PostJob/>
-                            <AllPost/>
-
-
-                            <div className="col-lg-3 pd-right-none no-pd">
-                                <div className="right-sidebar">
-                                    <TopJobs/>
-                                    <MostViews/>
-                                </div>
-                            </div>
-                            
-                        </div>
-                    </div>
+  render() {
+    return (
+      <main className="main-section">
+        <div className="container">
+          <div className="main-section-data">
+            <div className="row">
+              <div className="col-lg-3 col-md-4 pd-left-none no-pd">
+                <div>
+                  <UserProfiction />
                 </div>
-            </main>
-        )
-    }
+              </div>
+              <div className="col-lg-6 col-md-8 no-pd">
+                <div className="main-ws-sec">
+                  <PostJob />
+
+                  <AllPost />
+                </div>
+              </div>
+
+              <div className="col-lg-3 pd-right-none no-pd">
+                <div className="right-sidebar">
+                  <TopJobs />
+                  <MostViews />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
 
-export default Main
+export default Main;
