d555deb6968bf37e0c54108687a35bf6a61d2a7d
ldiur <lioneldiur@icloud.com>
2020-06-10 21:51:49 +0200

    Pop up 2, css issues

--- src/components/PostJobModal/PostJobModal.js
+++ src/components/PostJobModal/PostJobModal.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
-import MainPostJob from "../../container/MainPostJob/MainPostJob";
+//import MainPostJob from "../../container/MainPostJob/MainPostJob";
 
 function PostJobModal() {
   const [show, setShow] = useState(false);
@@ -9,7 +9,7 @@ function PostJobModal() {
   const handleShow = () => setShow(true);
 
   return (
-    <div>
+    <div className="post-project">
       <Button variant="primary" onClick={handleShow}>
         Post Job
       </Button>
@@ -18,8 +18,49 @@ function PostJobModal() {
         <Modal.Header closeButton>
           <Modal.Title>Post a job</Modal.Title>
         </Modal.Header>
+
         <Modal.Body>
-          <MainPostJob />
+          <div className="post-project-fields">
+            <form>
+              <div className="row justify-content-center">
+                <div className="col-lg-8">
+                  <input type="text" name="title" placeholder="Title" />
+                </div>
+                <div className="col-lg-8">
+                  <div className="inp-field">
+                    <select>
+                      <option>Category</option>
+                      <option>Category 1</option>
+                      <option>Category 2</option>
+                      <option>Category 3</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="col-lg-8">
+                  <input type="text" name="skills" placeholder="Skills" />
+                </div>
+                <div className="col-lg-8">
+                  <div className="price-sec">
+                    <div className="price-br">
+                      <input type="text" name="price1" placeholder="Price" />
+                      <i className="la la-dollar"></i>
+                    </div>
+                    <span>To</span>
+                    <div className="price-br">
+                      <input type="text" name="price1" placeholder="Price" />
+                      <i className="la la-dollar"></i>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-lg-8">
+                  <textarea
+                    name="description"
+                    placeholder="Description"
+                  ></textarea>
+                </div>
+              </div>
+            </form>
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
@@ -33,4 +74,5 @@ function PostJobModal() {
     </div>
   );
 }
+
 export default PostJobModal;
