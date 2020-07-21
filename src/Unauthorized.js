5e2d7ec044a650d178115a43e3f3c27e25e5bc05
Code4kongo <65186901+Code4kongo@users.noreply.github.com>
2020-07-21 07:54:42 +0200

    unauthorized page design

--- src/Unauthorized.js
+++ src/Unauthorized.js
@@ -4,13 +4,17 @@ import './Unauthorized.css';
 
 const Unauthorized = () => {
   return (
-    <div className='container'>
-        <h1>Unauthorized </h1> 
-      <div className="message">
-        <h1>403 - You Shall Not Pass</h1>
-      </div>
-      <p><Link to='/'>Back to Home</Link></p>
-    </div>
+   <div>
+         <h1 className="h1">404 Error Page </h1>
+          <section className="error-container">
+            <span>4</span>
+            <span><span className="screen-reader-text">0</span></span>
+            <span>4</span>
+          </section>
+          <div className="link-container">
+            <Link to="/" className="more-link">GO BACK HOME </Link>
+          </div>
+   </div>
   )
 }
 
